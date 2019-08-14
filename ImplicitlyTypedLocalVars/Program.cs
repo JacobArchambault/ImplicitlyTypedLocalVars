278c20d4ee9e591db8ece6fd5c62714c49faad0b
Jacob Archambault <jacob.archambault@waystar.com>
2019-08-14 16:57:35 -0400

    Add DeclareImplicitVars() method.

--- ImplicitlyTypedLocalVars/Program.cs
+++ ImplicitlyTypedLocalVars/Program.cs
@@ -10,6 +10,26 @@ namespace ImplicitlyTypedLocalVars
     {
         static void Main(string[] args)
         {
+            DeclareImplicitVars();
+
+            Console.ReadLine();
+        }
+
+        static void DeclareExplicitVars()
+        {
+            int myInt = 0;
+            bool myBool = true;
+            string myString = "Time, marches on...";
+        }
+        static void DeclareImplicitVars()
+        {
+            var myInt = 0;
+            var myBool = true;
+            var myString = "Time, marches on...";
+
+            Console.WriteLine($"myInt is a {myInt.GetType().Name}");
+            Console.WriteLine($"myBool is a {myBool.GetType().Name}");
+            Console.WriteLine($"myString is a {myString.GetType().Name}");
         }
     }
 }
